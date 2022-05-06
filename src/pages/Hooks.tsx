bb62447d2123615a62dee3139380dc8979b64867
tarasov <tarasov@reksoft.ru>
2022-05-06 16:03:27 +0300

    r1-userHooks Добавлены пользовательские хуки useInput, useHover

--- src/pages/Hooks.tsx
+++ src/pages/Hooks.tsx
@@ -2,22 +2,38 @@ import React, {useRef} from 'react';
 import useInput from "../hooks/useInput";
 import useHover from "../hooks/useHover";
 
-const Hooks = () => {
+const HookUseInput = () => {
     const username = useInput('');
     const password = useInput('');
-    const squareRef = useRef(null);
-    const isHovered = useHover(squareRef);
-
     return (
-        <div>
-            <h1>ТОП 5 ХУКОВ</h1>
+        <>
             <h2>Кастомный хук useInput:</h2>
             <input type="text" placeholder={'Username'} {...username}/>
             <input type="text" placeholder={'Password'} {...password}/>
             <button onClick={()=>console.log(username.value, password.value)}>console.log(username.value, password.value)</button>
+        </>
+    );
+}
 
+const HookUseHover = () => {
+    const squareRef = useRef(null);
+    const isHovered = useHover(squareRef);
+    return (
+        <>
             <h2>Кастомный хук useHover</h2>
             <div ref={squareRef} style={{width: '150px', height: '150px', backgroundColor: isHovered ? 'green' : 'tomato'}}/>
+        </>
+    );
+}
+
+const Hooks = () => {
+
+    return (
+        <div>
+            <h1>ТОП 5 ХУКОВ</h1>
+            <HookUseInput/>
+            <HookUseHover/>
+
         </div>
     );
 };
