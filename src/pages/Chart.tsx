50623f023ee561e1f7473f8751a7e451aa3614b1
ATarasov <temich080@gmail.com>
2022-12-29 01:39:21 +0300

    feature/ta/r1-1 misc

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -1,10 +1,24 @@
 import React, {useEffect, useState} from 'react';
+import { Gantt, Task, EventOption, StylingOption, ViewMode, DisplayOption } from 'gantt-task-react';
+import "gantt-task-react/dist/index.css";
 
 const Chart = () => {
-    return (
-        <div>
 
-        </div>
+    let tasks: Task[] = [
+        {
+            start: new Date(2020, 1, 1),
+            end: new Date(2020, 1, 2),
+            name: 'Idea',
+            id: 'Task 0',
+            type:'task',
+            progress: 45,
+            isDisabled: true,
+            styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+        },
+    ];
+
+    return (
+      <Gantt tasks={tasks} />
     );
 };
 
